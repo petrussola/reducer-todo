8ca6919d043048c7c9478efbbff7834b0497b750
Pere <pere.solaclaver@gmail.com>
2019-10-08 13:44:31 +0100

    Set state with useReducer importing reducers from file

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
+// REDUCERS
+
+import { initialState, reducer } from './reducers/reducer';
+
+// COMPONENTS
+
+
+
 function App() {
+  const [todos, dispatch] = useReducer(reducer, initialState)
+
+
   return (
     <div className="App">
       
